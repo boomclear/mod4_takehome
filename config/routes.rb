0f2b18583fff89c55cb539c638008e5f2d731e2d
Austin Woods <austinwoods99@gmail.com>
2023-09-13 05:28:38 -0500

    setup routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      post '/subscribe', to: 'subscription#new'
+      delete '/subscribe', to: 'subscription#cancel'
+      get '/subscribe', to: 'subscription#index'
+    end
+  end
 end
